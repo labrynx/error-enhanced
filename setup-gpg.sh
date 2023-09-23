a0bfaa3481efeac79c4beb9ed1ffc1faa9a5ef8f
alessbarb <alessbarb@gmail.com>
2023-09-23 02:03:53 +0200

    ci(semantic-release): Corrections
    Working with armored key

--- setup-gpg.sh
+++ setup-gpg.sh
@@ -4,11 +4,8 @@ set -e
 
 echo "Importing GPG key..."
 
-# Decode the GPG key
-echo "$GPG_BASE64" | base64 --decode > ./gpg_key.gpg
-
 # Import the GPG key
-gpg --batch --import ./gpg_key.gpg
+echo "$GPG_KEY" | gpg --batch --import
 
 # Configure Git to use the GPG key
 git config --global user.signingkey "$(gpg --list-secret-keys --keyid-format LONG --with-colons | grep sec | awk -F: '{print $5}')"
