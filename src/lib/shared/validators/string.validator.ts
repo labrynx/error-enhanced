7fb9b1e3a980f62b95a8e9fb8cb1901f81589403
alessbarb <alessbarb@gmail.com>
2023-10-08 11:47:58 +0200

    refactor(validators): refactor ValidString (so it works!)

--- src/lib/shared/validators/string.validator.ts
+++ src/lib/shared/validators/string.validator.ts
@@ -3,4 +3,8 @@ import z from 'zod';
 /**
  * Validates that a string is not empty.
  */
-export const ValidString = z.string().nonempty();
+export const ValidString = z
+  .string()
+  .refine(str => typeof str === 'string' && str.trim().length > 0, {
+    message: 'String must not be empty',
+  });
